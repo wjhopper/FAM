544f6a31657423056aa18ce49b9fae3ad36e060d
whopper-sculder <wjhopper510@gmail.com>
2015-08-18 09:46:19 -0400

    fixed bad reference to conditional accuracy data frame

--- R/LB4L_joint.R
+++ R/LB4L_joint.R
@@ -1,6 +1,7 @@
 #' @import dplyr
 #' @export
 LB4L_joint <- function(data) {
+
   jointAcc <- data  %>% filter(list != 1)  %>%
     group_by(subject, group, practice, other_type,
              prac_score,other_prac_acc,final_score) %>%
@@ -8,25 +9,30 @@ LB4L_joint <- function(data) {
 
   # I know from visual inspection that subject 20 has complete cases in each cell
   # So, use subject 20's data as a template to join by
-  joinerFrame <- filter(jointAcc,subject==20) %>%
-    ungroup() %>%
+  joinerFrame <- ungroup(filter(jointAcc,subject==20)) %>%
     select(practice,other_type,prac_score,other_prac_acc,final_score)
-  bigJoinerFrame <- cbind(subject = rep(unique(data$subject),
-                                        each=nrow(joinerFrame)),
-                          joinerFrame)
-  jointAcc <- left_join(bigJoinerFrame, jointAcc) %>%
+  jointAcc <- left_join(cbind(subject = rep(unique(data$subject),
+                                            each=nrow(joinerFrame)),
+                              joinerFrame),
+                        jointAcc) %>%
     group_by(subject) %>%
     mutate(group = group[!is.na(group)][1],
            cell_count = replace(cell_count, is.na(cell_count),0)) %>%
-    left_join(select(ungroup(conds_by_ss),
-                     subject,group,practice,other_type,grp_size=n),
-              by =c("subject","group","practice","other_type")) %>%
-    mutate(acc = cell_count/grp_size)
+    mutate(acc = cell_count/15)
 
   jointAcc_grouped <- jointAcc %>%
     group_by(group, practice, other_type, prac_score,
              other_prac_acc,final_score) %>%
     summarise(avgAcc = mean(acc))
 
-  return(subject = jointAcc, groups = jointAcc_grouped)
+  return(list(subject = jointAcc, groups = jointAcc_grouped))
 }
+
+#   left_join(select(ungroup(conds_by_ss),
+#                    subject,group,practice,other_type,grp_size=n),
+#             by =c("subject","group","practice","other_type")) %>%
+#    mutate(acc = cell_count/grp_size)
+
+#   bigJoinerFrame <- cbind(subject = rep(unique(data$subject),
+#                                         each=nrow(joinerFrame)),
+#                           joinerFrame)
