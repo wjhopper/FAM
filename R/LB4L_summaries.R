1e6e478427dc40416c2ff1218da57aa0af69b970
whopper-sculder <wjhopper510@gmail.com>
2015-08-19 08:59:28 -0400

    put all LB4L data summary functions in one file

--- R/LB4L_summaries.R
+++ R/LB4L_summaries.R
@@ -1,5 +1,75 @@
 #' @import dplyr
 #' @export
+LB4L_IV <- function(data) {
+
+  conds_by_ss <- data %>%   filter(list != 1) %>%
+    group_by(subject, group, practice,other_type)  %>%
+    summarize(prac_acc = mean(prac_score,na.rm=TRUE),
+              final_acc=mean(final_score),
+              n=n()) %>%
+    group_by(subject) %>%
+    mutate(chain=factor(1:n()))
+
+  conds_collapsed <- data %>%  filter(list != 1) %>%
+    group_by(group, practice,other_type)  %>%
+    summarize(prac_acc = mean(prac_score,na.rm=TRUE),
+              final_acc=mean(final_score),
+              n=n()) %>%
+    group_by(group) %>%
+    mutate(chain=factor(1:n()))
+
+  return(list(subject= conds_by_ss, groups = conds_collapsed))
+}
+
+
+#' @import dplyr
+#' @export
+LB4L_joint <- function(data) {
+
+  columns <- c("prac_score", "other_prac_acc")
+  data[,columns] <- lapply(data[,columns],factor,exclude=NULL)
+
+  jointAcc <- data  %>% filter(list != 1)  %>%
+    group_by(subject, group, practice, other_type,
+             prac_score,other_prac_acc,final_score) %>%
+    summarise(cell_count = n())
+
+  # I know from visual inspection that subject 20 has complete cases in each cell
+  # So, use subject 20's data as a template to join by
+  joinerFrame <- ungroup(filter(jointAcc,subject==20)) %>%
+    select(practice,other_type,prac_score,other_prac_acc,final_score)
+  jointAcc <- left_join(cbind(subject = rep(unique(data$subject),
+                                            each=nrow(joinerFrame)),
+                              joinerFrame),
+                        jointAcc) %>%
+    group_by(subject) %>%
+    mutate(group = group[!is.na(group)][1],
+           cell_count = replace(cell_count, is.na(cell_count),0)) %>%
+    mutate(acc = cell_count/15,
+           merged_prac_score = ifelse(is.na(levels(prac_score)[prac_score]),
+                                      as.numeric(levels(other_prac_acc))[other_prac_acc],
+                                      as.numeric(levels(prac_score))[prac_score]))
+  jointAcc$merged_prac_score <- factor(jointAcc$merged_prac_score)
+
+  jointAcc_grouped <- jointAcc %>%
+    group_by(group, practice, other_type, prac_score,
+             other_prac_acc,final_score) %>%
+    summarise(avgAcc = mean(acc))
+
+  return(list(subject = jointAcc, groups = jointAcc_grouped))
+}
+
+#   left_join(select(ungroup(conds_by_ss),
+#                    subject,group,practice,other_type,grp_size=n),
+#             by =c("subject","group","practice","other_type")) %>%
+#    mutate(acc = cell_count/grp_size)
+
+#   bigJoinerFrame <- cbind(subject = rep(unique(data$subject),
+#                                         each=nrow(joinerFrame)),
+#                           joinerFrame)
+
+#' @import dplyr
+#' @export
 LB4L_conditional <- function(data) {
   columns <- c("prac_score", "other_prac_acc")
   data[,columns] <- lapply(data[,columns],factor,exclude=NULL)
