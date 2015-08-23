42105b2eb8e8aeea3d4a089de68eb6b8427f7bb6
whopper-sculder <wjhopper510@gmail.com>
2015-08-23 14:36:10 -0400

    changed data typing to factors in the raw data and fixed accidental NA

--- data-raw/SCKT.R
+++ data-raw/SCKT.R
@@ -16,14 +16,14 @@ rawData <- do.call(rbind,rawData) %>%
          final_score = score(target,final_resp))
 
 # There never was a practice test for these so fix them to NA
-rawData$prac_score[rawData$pratice %in% c("C","S")]  <- NA
+rawData$prac_score[rawData$practice %in% c("C","S")]  <- NA
 
-cleaned1 <- edit(rawData[rawData$prac_score %in% 2, c("subject","target",
+SCKTprac_clean <- edit(rawData[rawData$prac_score %in% 2, c("subject","target",
                                                       "prac_resp","prac_score")])
-cleaned2 <- edit(rawData[rawData$final_score %in% 2, c("subject","target",
+SCKTfinal_clean <- edit(rawData[rawData$final_score %in% 2, c("subject","target",
                                                        "final_resp","final_score")])
-rawData$prac_score[rawData$prac_score %in% 2] <- cleaned1$prac_score
-rawData$final_score[rawData$final_score %in% 2] <- cleaned2$final_score
+rawData$prac_score[rawData$prac_score %in% 2] <- SCKTprac_clean$prac_score
+rawData$final_score[rawData$final_score %in% 2] <- SCKTfinal_clean$final_score
 
 rawData <- rawData %>% group_by(subject,target) %>%
   # Add identifier and score for the other half of the repeating target 'pair'
@@ -31,6 +31,11 @@ rawData <- rawData %>% group_by(subject,target) %>%
          other_type =  if (n() > 1) { rev(practice) } else { NA_character_},
          other_final = if (n() > 1) { rev(final_score) } else { NA_real_})
 
+cols<- colnames(rawData) %in% c("subject","group", "half", "list", "practice",
+                             "repeating","condition", "cond", "target",
+                             "other_prac","other_final","other_type")
+rawData[cols] <- lapply(rawData[cols], factor)
 
 # Save as binary rda
-# save(rawData, file = file.path("data","SCKT_allSs.rda"))
+# SCKT_allSs <- ungroup(rawData)
+# save(SCKT_allSs, file = file.path("data","SCKT_allSs.rda"))
