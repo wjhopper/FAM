6ab049a0986c932bf5a13721f030c3dc8679e72b
whopper-sculder <wjhopper510@gmail.com>
2015-09-02 16:04:23 -0400

    version .01 Main change is to  measure RT's realtive to previous item output, even if that item is intrusion or duplicate, in the vignette.
    Several bug fixes for the raw data also. Zero length reponses in the middle of output
    lists are now sorted to bottom of list before calculating RT (fixes #2) and zero length reposnes
    now dont count towards repeats or intrusions (fixes #1).

--- data-raw/CFR.R
+++ data-raw/CFR.R
@@ -50,6 +50,7 @@ study_data <- data.frame(do.call(rbind,
 
 rawData <- rbind(study_data, prac_data,final_data)
 rawData$subject[rawData$subject==55] <- 20
+rawData$score <- NA_real_
 rawData <- arrange(rawData, subject) %>%
   group_by(subject,list,phase) %>%
   mutate(practice = practice[1],
@@ -60,19 +61,20 @@ rawData <- arrange(rawData, subject) %>%
          LP = replace(LP, !is.finite(LP) | LP ==0, NA),
          resp_dur = LP-FP) %>%
   group_by(subject,list,phase) %>%
+  arrange(FP) %>%
   mutate(RT = c(FP[1]-onset[1],diff(FP)-resp_dur[1:n()-1]),
          CRT = cumsum(RT),
-         score = if (phase[1]=='study'| (phase[1]=='prac' & practice[1]=='S')) {
-                   return(NA_real_)
+         score = if (phase[1]=='final'| (phase[1]=='prac' & practice[1]=='T')) {
+                   whoppeR::score(target,resp,checkDuplicates = TRUE,
+                         ignoreVals=NA_real_)
                  } else {
-                   as.numeric(score(target,resp,checkDuplicates = TRUE,
-                         ignoreVals=NA))
+                   NA_real_
                  },
-         where = if (phase[1]=='study'| (phase[1]=='prac' & practice[1]=='S')) {
-           return(NA_real_)
-         } else {
+         where = if (phase[1]=='final'| (phase[1]=='prac' & practice[1]=='T')) {
            score(target,resp,checkDuplicates = TRUE,
                  ignoreVals=NA,index.return = TRUE)$position
+         } else {
+           NA_real_
          })
 CFRcleaned <- filter(rawData,any(score == 2)) %>%
   select(subject,target,resp, score, where) %>%
@@ -85,19 +87,22 @@ CFRcleaned <- filter(rawData,any(score == 2)) %>%
 #   of "floor" in the same list", set it to 0
 # Note: I know from visual inspection that "canon" (row 73) is a duplicate
 #   of "cannon" in the same list", set it to 0
-
+# Note: I know "staek" in the rawData data frme (row 1358) is a correct
+#   answer, dunno how it got missed in fuzzy matching
+rawData$score[rawData$resp=='staek']<-1
 
 # Replace the fuzzy matches with the manual decisions
 rawData$score[rawData$score %in% 2] <- CFRcleaned$score
+rawData$final_order <- NULL # useless column, redundant with order column
 
 rawData <- rawData %>%
   group_by(subject,list,phase) %>%
   mutate(repeats = if (phase[1]=='study'| (phase[1]=='prac' & practice[1]=='S')) {
            return(NA_real_)
          } else {
-           as.numeric(duplicated(resp,incomparables = NA))
+           as.numeric(duplicated(resp,incomparables = c(NA_character_,'')))
          },
-         intrusions  = as.numeric(score==0 & repeats ==0 & response != '')) %>%
+         intrusions  = as.numeric(score==0 & repeats ==0 & resp != '')) %>%
   group_by(subject,phase,practice) %>%
   mutate(cond_list =  rep(1:length(unique(list)),as.vector(table(list))))
 
